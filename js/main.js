f6b1a916bf4f3cdc6871accdf26060427ea37c25
marbiru <code@uribram.com>
2016-02-15 11:50:50 +0630

    dummy update to new year dates
    new year dates update with year input using a keyup function, but not
    to actual new yeardate

--- js/main.js
+++ js/main.js
@@ -20,6 +20,33 @@ var zodiacArray = [
 "Goat",
 ]
 
+var newYearDateArray = [
+"17th February",
+"6th February",
+"26th January",
+"14th February",
+"3rd February",
+"23rd January",
+"11th February",
+"31st January",
+"19th February",
+"8th February",
+"28th January",
+"15th February",
+"5th February",
+"24th January",
+"12th February",
+"2nd February",
+"22nd January",
+"9th February",
+"29th January",
+]
+
+$( "input").on('keyup', function() {
+    printNewYearDate.innerHTML = newYearDateArray[0];
+    printNewYearDate2.innerHTML = "hello";
+});
+
 $( "#before" ).click(function () {
 
 	var inputYear = $( "#inputYear" ).val();
